3bd6ba40d5fe8e23e64a39080ea8956a8408a2f6
Artur Laskowski <laskowski.artur.96@gmail.com>
2018-07-10 11:48:13 +0200

    initial commit

---
+++ Assets/missileShot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missileShot : MonoBehaviour {
+
+    public GameObject missile;
+    public GameObject missileLauncher;
+
+    private List<GameObject> spawnedMissiles;
+
+	// Use this for initialization
+	void Start () {
+        spawnedMissiles = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SpawnMissile();
+        }
+
+        spawnedMissiles.ForEach((GameObject m) => m.transform.position += new Vector3(0, 0.1f, 0));
+    }
+
+    //void UpdateMissile
+
+    void SpawnMissile()
+    {
+        var newMissile = Instantiate(missile, transform.position, Quaternion.identity);
+        spawnedMissiles.Add(newMissile);
+    }
+}
---
+++ Assets/targetBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 transform = new Vector3();
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform += new Vector3(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform += new Vector3(0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            transform += new Vector3(0, -0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform += new Vector3(0, 0.1f, 0);
+        }
+
+        this.transform.position += transform;
+
+    }
+}
