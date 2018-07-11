665b9668aa243cf02019741644ef99f051268d0a
Artur Laskowski <laskowski.artur.96@gmail.com>
2018-07-11 13:14:20 +0200

    file names changes

---
+++ Assets/scripts/EnemyBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    private float startTime;
+    private float speed;
+    private float journeyLength;
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    ScoreHandlerBehavior scoreHandler;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+        speed = 5;
+        startPos = this.transform.position;
+        endPos  = new Vector3(Random.Range(-20.0f, 20.0f), -1, 0);
+        journeyLength = Vector3.Distance(startPos, endPos);
+
+        scoreHandler = ScoreHandlerBehavior.Instance;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+
+        if (IsCollidingWithGround()) {
+            DecreaseHealth();
+            DestroyEnemy();
+        }
+    }
+
+    void Move() {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        this.transform.position = Vector3.Lerp(startPos, endPos, fracJourney);
+    }
+
+    bool IsCollidingWithGround() {
+        return this.transform.position.y < 0;
+    }
+
+    void DestroyEnemy() {
+        Destroy(this.gameObject);
+    }
+
+    public void ExplodeEnemy() {
+        DestroyEnemy();
+        scoreHandler.ChangeScore(+1);
+    }
+
+    void DecreaseHealth() {
+        scoreHandler.ChangeHealth(-1);
+    }
+}
---
+++ Assets/scripts/MissileTubeBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileTubeBehavior : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject parentLauncher;
+
+    public Transform muzzle;
+
+    public GameObject missile;
+    public GameObject targetMarker;
+
+    public float LastShotTime { get; set; }
+    public int RoundsPerMinute { get; set; }
+
+    // Use this for initialization
+    void Start () {
+        this.transform.SetParent(parentLauncher.transform);
+
+        LastShotTime = Time.time;
+        RoundsPerMinute = 6000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SetRotation(transform.position, target.transform.position);
+
+        HandleUserInput();
+    }
+
+    void SetRotation(Vector3 start, Vector3 end) {
+        var dir = end - start;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+    }
+
+    bool CanSpawnMissile() {
+        float interval = 60.0f / RoundsPerMinute;
+        return Time.time > LastShotTime + interval;
+    }
+
+    void HandleUserInput() {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0)) {
+            if (CanSpawnMissile()) {
+                SpawnMissile();
+            }
+        }
+    }
+
+    void SpawnMissile() {
+        GameObject missileObject = Instantiate(missile, muzzle.position, this.transform.rotation);
+        MissileBehavior mb = missileObject.GetComponent<MissileBehavior>();
+        mb.TargetMarkerObject = CreateTargetMarker();
+
+        LastShotTime = Time.time;
+    }
+
+    GameObject CreateTargetMarker() {
+        Transform targetTransform = GameObject.Find("target").transform;
+        GameObject targetMarkerObject = Instantiate(targetMarker, targetTransform.position, Quaternion.identity);
+        return targetMarkerObject;
+    }
+}
